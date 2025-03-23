33bd2ee364f14bbfc6d2e14cfba8e427e8768737
Lynes <izyosirdevinchii@gmail.com>
2025-03-23 22:19:38 +0300

    Changed public URL

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,6 @@ const eslintConfig = [
     rules: {
       "react/no-escaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
